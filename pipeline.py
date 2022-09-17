764baed3de8fdc2b28918937a3c7ad972db35506
Ahmet Burak Eren <91292963+abetr@users.noreply.github.com>
2022-09-17 22:13:32 +0200

    Update pipeline.py

--- pipeline.py
+++ pipeline.py
@@ -1,11 +1,30 @@
 pipeline {
     agent any
     stages {
-        stage('run') {
+        stage('build') {
+            steps {
+                echo "Clarusway_Way to Reinvent Yourself"
+                sh 'echo using shell within Jenkinsfile'
+                echo 'not using shell in the Jenkinsfile'
+            }
+        }
+        stage('Jenkinsfile') {
+            steps {
+                echo 'Clarusway_Way to Reinvent Yourself'
+                sh 'echo Integrating Jenkins Pipeline with GitHub Webhook using Jenkinsfile'
+            }
+        }
+        stage('Run Pyhton Code') {
             steps {
                 echo 'Clarusway_Way to Reinvent Yourself'
                 sh 'python --version'
                 sh 'python pipeline.py'
+            }
+        }
+        stage('Stage-5') {
+            steps {
+                echo 'Clarusway_Way to Reinvent Yourself'
+                sh 'echo Integrating Jenkins Pipeline with GitHub Webhook using Jenkinsfile'
             }
         }
     }
